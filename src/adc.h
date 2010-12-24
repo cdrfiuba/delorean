45b8ea6462badcf77e6a6a0c6e302eb9bc705db2
Ariel Burman <arielburman@gmail.com>
2010-12-24 16:03:45 -0300

    * Modifique la calibracion. Primero los sensores laterales sobre la linea y el central no. Luego al reves. Usa los valores analogicos y los guarda en la EEPROM. * Modifique el makefile con algunas opciones para la EEPROM y fundamental la optimizacion -O2.

--- src/adc.h
+++ src/adc.h
@@ -3,6 +3,7 @@
 
 //#include "delorean.h"
 #include "board.h"
+#include <avr/eeprom.h>
 
 // Si se quiere utilizar los conversores en el modo de Interrupcion
 // dejar el siguiente define. En caso de que se quiera trabajar con la
@@ -10,10 +11,6 @@
 #define _ADC_MODO_INT_
 
 
-void configurarADCs(void);
-void capturarADc(void);
-void calibrarNiveles(void);
-void capturarADcPRO(void);
 
 volatile uint8_t analogSensorIzq;
 volatile uint8_t analogSensorCen;
@@ -21,19 +18,109 @@ volatile uint8_t analogSensorDer;
 
 // Variables del máximo que representa el blanco, el mínimo que representa el negro y cual de los dos es la linea.
 // colorLinea tiene 1 si la linea es blanca y 0 si la linea es negra
-uint16_t minNivelSensor, maxNivelSensor, colorLinea;
+volatile uint8_t colorLinea;
+
+#define SI(num)	(num<<4)
+#define SC(num)	(num<<2)
+#define SD(num)	(num<<0)
 
 typedef enum{
-    ES_000 = 0,
-    ES_001 = 1,
-    ES_010 = 2,
-    ES_011 = 3,
-    ES_100 = 4,
-    ES_110 = 6 
+		ES_333 = SI(3) | SC(3) | SD(3),
+		ES_332 = SI(3) | SC(3) | SD(2),
+		ES_331 = SI(3) | SC(3) | SD(1),
+		ES_330 = SI(3) | SC(3) | SD(0),
+		ES_323 = SI(3) | SC(2) | SD(3),
+		ES_322 = SI(3) | SC(2) | SD(2),
+		ES_321 = SI(3) | SC(2) | SD(1),
+		ES_320 = SI(3) | SC(2) | SD(0),
+		ES_313 = SI(3) | SC(1) | SD(3),
+		ES_312 = SI(3) | SC(1) | SD(2),
+		ES_311 = SI(3) | SC(1) | SD(1),
+		ES_310 = SI(3) | SC(1) | SD(0),
+		ES_303 = SI(3) | SC(0) | SD(3),
+		ES_302 = SI(3) | SC(0) | SD(2),
+		ES_301 = SI(3) | SC(0) | SD(1),
+		ES_300 = SI(3) | SC(0) | SD(0),
+		ES_233 = SI(2) | SC(3) | SD(3),
+		ES_232 = SI(2) | SC(3) | SD(2),
+		ES_231 = SI(2) | SC(3) | SD(1),
+		ES_230 = SI(2) | SC(3) | SD(0),
+		ES_223 = SI(2) | SC(2) | SD(3),
+		ES_222 = SI(2) | SC(2) | SD(2),
+		ES_221 = SI(2) | SC(2) | SD(1),
+		ES_220 = SI(2) | SC(2) | SD(0),
+		ES_213 = SI(2) | SC(1) | SD(3),
+		ES_212 = SI(2) | SC(1) | SD(2),
+		ES_211 = SI(2) | SC(1) | SD(1),
+		ES_210 = SI(2) | SC(1) | SD(0),
+		ES_203 = SI(2) | SC(0) | SD(3),
+		ES_202 = SI(2) | SC(0) | SD(2),
+		ES_201 = SI(2) | SC(0) | SD(1),
+		ES_200 = SI(2) | SC(0) | SD(0),
+		ES_133 = SI(1) | SC(3) | SD(3),
+		ES_132 = SI(1) | SC(3) | SD(2),
+		ES_131 = SI(1) | SC(3) | SD(1),
+		ES_130 = SI(1) | SC(3) | SD(0),
+		ES_123 = SI(1) | SC(2) | SD(3),
+		ES_122 = SI(1) | SC(2) | SD(2),
+		ES_121 = SI(1) | SC(2) | SD(1),
+		ES_120 = SI(1) | SC(2) | SD(0),
+		ES_113 = SI(1) | SC(1) | SD(3),
+		ES_112 = SI(1) | SC(1) | SD(2),
+		ES_111 = SI(1) | SC(1) | SD(1),
+		ES_110 = SI(1) | SC(1) | SD(0),
+		ES_103 = SI(1) | SC(0) | SD(3),
+		ES_102 = SI(1) | SC(0) | SD(2),
+		ES_101 = SI(1) | SC(0) | SD(1),
+		ES_100 = SI(1) | SC(0) | SD(0),
+		ES_033 = SI(0) | SC(3) | SD(3),
+		ES_032 = SI(0) | SC(3) | SD(2),
+		ES_031 = SI(0) | SC(3) | SD(1),
+		ES_030 = SI(0) | SC(3) | SD(0),
+		ES_023 = SI(0) | SC(2) | SD(3),
+		ES_022 = SI(0) | SC(2) | SD(2),
+		ES_021 = SI(0) | SC(2) | SD(1),
+		ES_020 = SI(0) | SC(2) | SD(0),
+		ES_013 = SI(0) | SC(1) | SD(3),
+		ES_012 = SI(0) | SC(1) | SD(2),
+		ES_011 = SI(0) | SC(1) | SD(1),
+		ES_010 = SI(0) | SC(1) | SD(0),
+		ES_003 = SI(0) | SC(0) | SD(3),
+		ES_002 = SI(0) | SC(0) | SD(2),
+		ES_001 = SI(0) | SC(0) | SD(1),
+		ES_000 = SI(0) | SC(0) | SD(0)
     } estado_sensor_t;
 
-estado_sensor_t estadoSensores;
 
+void configurarADCs(void);
+void capturarADc(void);
+uint8_t calibrarNiveles(void);
+void calcularNiveles(void);
+void capturarADcPRO(void);
 estado_sensor_t analizarSensores(void);
+
+volatile uint8_t sdNivelMedio, sdNivelUmbralP, sdNivelUmbralN;
+volatile uint8_t scNivelMedio, scNivelUmbralP, scNivelUmbralN;
+volatile uint8_t siNivelMedio, siNivelUmbralP, siNivelUmbralN;
+
+//#define S_UMBRAL_CTE	3 
+
+#define SD_NO_LINE_EEP_ADDR 2
+#define SC_NO_LINE_EEP_ADDR 3
+#define SI_NO_LINE_EEP_ADDR 4
+#define SD_LINE_EEP_ADDR 5
+#define SC_LINE_EEP_ADDR 6
+#define SI_LINE_EEP_ADDR 7
+
+#define MODO_EEPADDR 0
+#define MODO_VALUE_EEPCLEAN 0
+#define MODO_VALUE_START 0xFF
+#define MODO_VALUE_LINEDEF 1
+#define MODO_VALUE_COMPLETE 2
+#define MODO_VALUE_ERROR 3
+
+#define LINEA_EEPADDR 1
+#define LINEA_BLANCA 1
+#define LINEA_NEGRA 2
 
 #endif
