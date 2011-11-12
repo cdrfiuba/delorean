da9b3e117ff8efed28018cd2f7e19ea1be75376b
Ariel Burman <arielburman@gmail.com>
2011-11-12 00:41:00 -0300

    junte lo que hicimos con seb, hay que testear

--- src/sensores.c
+++ src/sensores.c
@@ -1,6 +1,6 @@
 #include "sensores.h"
 
-//volatile bool sensor_est_nuevo;
+volatile bool sensor_est_nuevo;
 volatile sensor_t sensores;
 
 /* Funcionamiento con "Pin Change" */
@@ -11,18 +11,18 @@ void configurarSensores(){
   EmisorIzqOn();
   EmisorDerOn();
 
-/*  
+  
   PCICR = (1<<PCIE1); //Habilita interrupcion de pin change
   PCMSK1 = (1<<PCINT9) | (1<<PCINT8); //Indica que solo estos dos pines disparan la interrupcion
   sensor_est_nuevo = false;
   sensores = LINEA;
-*/
+
 }
 
-/*
+
 ISR(PCINT1_vect,ISR_NAKED){ //,ISR_NAKED
   sensores = (PIN_SDRE & PIN_SENSORES_MASK);
   sensor_est_nuevo = true;
   Reti();
 }
-*/
+
