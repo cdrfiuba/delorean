3a181dc7687ef73e54043b347d1a4e3afbd0fade
Ariel Burman <arielburman@gmail.com>
2014-05-01 17:28:48 -0300

    No se que es este codigo pero no lo quiero borrar

--- src/motores.h
+++ src/motores.h
@@ -1,45 +1,80 @@
-#ifndef _MOTORES_
-#define _MOTORES_
+#ifndef _MOTORES_H_
+#define _MOTORES_H_
 
-#include "board.h"
+#include <avr/io.h>
 #include "definiciones.h"
 
-// Completar cuales reducciones corresponden a A y cuales a B
-//#define _REDUCCION_A_
-#define _REDUCCION_B_
+/* Definiciones de los pines correspondientes a los motores */
 
-#define velocidadMD OCR2A
-#define velocidadMI OCR2B
+/*MD_PWM = Motor Derecho (A) Enable (Enable)*/
+#define PORT_MD_EN_NAME  B
+#define MD_EN_NUMBER     1
+/*MI_WPM = Motor Izquierdo PWM (Enable)*/
+#define PORT_MI_EN_NAME  B
+#define MI_EN_NUMBER     2
 
-#define PWM_VMEDIO  0x80
+#define PORT_MD_EN     def_port_reg(PORT_MD_EN_NAME)
+#define PIN_MD_EN      def_pin_reg(PORT_MD_EN_NAME)
+#define DDR_MD_EN      def_ddr_reg(PORT_MD_EN_NAME)
 
-#define PRESCALER_PWM_OFF	(0<<CS22)|(0<<CS21)|(0<<CS20)
-// y el "prescaler" en 1 (CS12 = 0 , CS11 =0 , CS10 = 1)
-#define PRESCALER_PWM_ON	(0<<CS22)|(1<<CS21)|(0<<CS20)
+#define PORT_MI_EN     def_port_reg(PORT_MI_EN_NAME)
+#define PIN_MI_EN      def_pin_reg(PORT_MI_EN_NAME)
+#define DDR_MI_EN      def_ddr_reg(PORT_MI_EN_NAME)
 
-// TCCR1A -->  COM1A = 00 , COM1B = 00 y WGM = 14 (para usar un TOP fijo)
-#define PWM_TCCR2A ( (0<<COM2A1) | (0<<COM2A0) | (0<<COM2B1) | (0<<COM2B0) | (0<<WGM21) | (1<<WGM20) )
-#define PWM_TCCR2B ( (0<<WGM22) | PRESCALER_PWM_OFF )
 
-// Interrupciones en comparaciones
-#define TIMSK_VALUE  ( (0<<TOIE2) | (1<<OCIE2A) | (1<<OCIE2B) )
+/*MI_IN1 = Motor Izquierdo IN1*/
+#define PORT_MI_IN1_NAME  C
+#define MI_IN1_NUMBER     4
+/*MI_IN2 = Motor Izquierdo IN2*/
+#define PORT_MI_IN2_NAME	 C
+#define MI_IN2_NUMBER		   5
 
-// Metodo para prender y apagar los PWM. Pone o saca el prescaler. (OJO ESTAMOS
-// PISANDO EL WMGM22. Si este no es cero hay que arreglar estas dos lineas
-// El problema esta en los bits 6 y 7. Ver la hoja de datos.
-#define PwmStart() (TCCR2B = PRESCALER_PWM_ON)
-#define PwmStop()  (TCCR2B = PRESCALER_PWM_OFF)
+/*MD_IN1 = Motor Derecho IN1*/
+#define PORT_MD_IN1_NAME  D
+#define MD_IN1_NUMBER     0
+/*MD_IN2 = Motor Right IN2*/
+#define PORT_MD_IN2_NAME  D
+#define MD_IN2_NUMBER     1
 
+#define PORT_MD_IN1     def_port_reg(PORT_MD_IN1_NAME)
+#define PIN_MD_IN1      def_pin_reg(PORT_MD_IN1_NAME)
+#define DDR_MD_IN1      def_ddr_reg(PORT_MD_IN1_NAME)
+
+#define PORT_MD_IN2     def_port_reg(PORT_MD_IN2_NAME)
+#define PIN_MD_IN2      def_pin_reg(PORT_MD_IN2_NAME)
+#define DDR_MD_IN2      def_ddr_reg(PORT_MD_IN2_NAME)
+
+#define PORT_MI_IN1     def_port_reg(PORT_MI_IN1_NAME)
+#define PIN_MI_IN1      def_pin_reg(PORT_MI_IN1_NAME)
+#define DDR_MI_IN1      def_ddr_reg(PORT_MI_IN1_NAME)
+
+#define PORT_MI_IN2     def_port_reg(PORT_MI_IN2_NAME)
+#define PIN_MI_IN2      def_pin_reg(PORT_MI_IN2_NAME)
+#define DDR_MI_IN2      def_ddr_reg(PORT_MI_IN2_NAME)
+
+/* Servo Motor*/
+#define PORT_SERVO_NAME  B
+#define SERVO_NUMBER     6
+
+#define PORT_SERVO     def_port_reg(PORT_SERVO_NAME)
+#define PIN_SERVO      def_pin_reg(PORT_SERVO_NAME)
+#define DDR_SERVO      def_ddr_reg(PORT_SERVO_NAME)
+
+
+/* Macros */
+#define apagarMotores() {ClearBit(PORT_MD_EN, MD_EN_NUMBER);ClearBit(PORT_MI_EN, MI_EN_NUMBER);}
+#define encenderMotores() {SetBit(PORT_MD_EN, MD_EN_NUMBER);SetBit(PORT_MI_EN, MI_EN_NUMBER);}
+
+/*-----------------------------------------------*/
 
 void motoresApagar(void);
 void motoresEncender(void);
 void configurarMotores(void);
 
-void Avanzar();
-void Detenido();
-void GirarIzquierda();
-void GirarDerecha();
-void CorreccionIzquierda();
-void CorreccionDerecha();
+void motoresAvanzar();
+void motoresRetroceder();
+void motoresGirarIzquierda();
+void motoresGirarDerecha();
 
+void configurarMotorPolleras();
 #endif
